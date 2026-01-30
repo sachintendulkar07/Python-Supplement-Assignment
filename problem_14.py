370e28bd212e600917fe7b6981ed1c6280cb66b3
sachintendulkar07 <sachintendulkar.a_25ec@kgkite.ac.in>
2026-01-30 13:50:39 +0530

    Update problem_14.py

--- problem_14.py
+++ problem_14.py
@@ -1,11 +1,11 @@
 # Problem 14: Check if a number is prime
 # Find and fix the error
 
-def is_prime(n):
-    if n < 2:
+def is_prime(number):
+    if number < 2:
         return False
-    for i in range(2, n):
-        if n % i == 0:
+    for i in range(2, number):
+        if number % i == 0:
             return False
     return True
 
