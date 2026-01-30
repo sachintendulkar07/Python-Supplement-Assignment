00ec7d0f10bfdf2b7f0504c26372efc27d3906a6
sachintendulkar07 <sachintendulkar.a_25ec@kgkite.ac.in>
2026-01-30 14:10:32 +0530

    Update problem_22.py

--- problem_22.py
+++ problem_22.py
@@ -1,7 +1,7 @@
 # Problem 22: Flatten a nested list
 # Find and fix the error
 
-nested_list = [[1, 2], [3, 4], [5, 6]]
+nested_list = [[2, 3], [4, 5], [6, 7]]
 flat_list = []
 for sublist in nested_list:
     for item in sublist:
