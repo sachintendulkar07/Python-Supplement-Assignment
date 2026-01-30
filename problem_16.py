6e7c8a4b8a28d62d6961b0c57d8360b634a6459a
sachintendulkar07 <sachintendulkar.a_25ec@kgkite.ac.in>
2026-01-30 13:55:26 +0530

    Update problem_16.py

--- problem_16.py
+++ problem_16.py
@@ -1,7 +1,7 @@
 # Problem 16: Find the second largest number in a list
 # Find and fix the error
 
-num = [45, 89, 12, 78, 34]
-num.sort()
-second_largest = num[-2]
+numbers = [55, 89, 12, 78, 34]
+numbers.sort()
+second_largest = numbers[-2]
 print(f"Second largest: {second_largest}")
