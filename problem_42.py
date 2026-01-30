de0e9c39d6cc02d658bf9ab3f20dfcf15dae321d
sachintendulkar07 <sachintendulkar.a_25ec@kgkite.ac.in>
2026-01-30 15:19:39 +0530

    Update problem_42.py

--- problem_42.py
+++ problem_42.py
@@ -1,7 +1,7 @@
 # Problem 42: Convert list to string
 # Find and fix the error
 
-words = ["Hello", "World", "Python"]
+words = ["Hello","World","Python"]
 sentence = ""
 for word in words:
     sentence += word + " "
